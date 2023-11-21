a8610b662ebafd3b16f3440156f138869b300bae
Anhelina Babareka <anhielina.babareka@gmail.com>
2023-11-21 12:56:08 +0300

    checkout beforeEach refactor

--- TestLogic/Checkout/CheckoutTest.spec.ts
+++ TestLogic/Checkout/CheckoutTest.spec.ts
@@ -8,10 +8,10 @@ test.describe('Checkout tests', () => {
 
     test.beforeEach(async ({pageManager, addItemToCart}) => {
         await pageManager.cartPage.open();
+        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
     })
 
     test('Checkout process should be proceeded successfully', async ({pageManager}) => {
-        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
         await pageManager.checkoutPage.fillUserInfo(user);
         await expect(pageManager.checkoutOverviewPage.Summary.SubtotalLabel).toHaveText(`Item total: ${item.price}`)
         await expect(pageManager.checkoutOverviewPage.Summary.TaxLabel).toHaveText(`Tax: ${item.tax}`)
@@ -19,40 +19,34 @@ test.describe('Checkout tests', () => {
     })
 
     test('User cannot proceed to checkout overview page with empty form', async ({pageManager}) => {
-        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
         await pageManager.checkoutPage.continueToOverviewPage();
         await expect(pageManager.checkoutPage.Form.ErrorMessage).toHaveText(errorMessage.errorCheckoutEmptyFirstName);
     })
 
     test('User cannot proceed to checkout overview page with only first name filled in', async ({pageManager}) => {
-        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
         await pageManager.checkoutPage.fillInFirstName(user.firstName);
         await pageManager.checkoutPage.continueToOverviewPage();
         await expect(pageManager.checkoutPage.Form.ErrorMessage).toHaveText(errorMessage.errorCheckoutEmptyLastName);
     })
 
     test('User cannot proceed to checkout overview page with only last name filled in', async ({pageManager}) => {
-        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
         await pageManager.checkoutPage.fillInLastName(user.lastName);
         await pageManager.checkoutPage.continueToOverviewPage();
         await expect(pageManager.checkoutPage.Form.ErrorMessage).toHaveText(errorMessage.errorCheckoutEmptyFirstName);
     })
 
     test('User cannot proceed to checkout overview page with only postal code filled in', async ({pageManager}) => {
-        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
         await pageManager.checkoutPage.fillInPostalCode(user.postalCode);
         await pageManager.checkoutPage.continueToOverviewPage();
         await expect(pageManager.checkoutPage.Form.ErrorMessage).toHaveText(errorMessage.errorCheckoutEmptyFirstName);
     })
 
     test('Checkout Cancel button redirects to Cart page', async ({pageManager}) => {
-        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
         await pageManager.checkoutPage.returnToCartPage()
         await expect(pageManager.cartPage.Header.PageTitle).toBeVisible()
     })
 
     test('Checkout Overview page shows correct item information', async ({pageManager}) => {
-        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
         await pageManager.checkoutPage.fillUserInfo(user);
         await expect(pageManager.checkoutOverviewPage.Item.CartItemName.first()).toHaveText(item.name);
         await expect(pageManager.checkoutOverviewPage.Item.CartItemDescription.first()).toHaveText(item.description);
@@ -62,7 +56,6 @@ test.describe('Checkout tests', () => {
     // Bug: redirects to Catalog instead
     test('Checkout Overview Cancel button redirects to Checkout page', async ({pageManager}) => {
         test.fail();
-        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
         await pageManager.checkoutPage.fillUserInfo(user);
         await pageManager.checkoutOverviewPage.returnToCheckoutPage();
         await expect(pageManager.checkoutPage.Form.FirstNameInput).toBeVisible();
@@ -74,7 +67,6 @@ test.describe('Checkout tests', () => {
         const pageTitle = 'Thank you for your order!';
         const pageDescription = 'Your order has been dispatched, and will arrive just as fast as the pony can get there!';
 
-        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
         await pageManager.checkoutPage.fillUserInfo(user);
         await pageManager.checkoutOverviewPage.finalizeOrder()
         await expect(pageManager.checkoutCompletePage.CheckoutCompleteContainer.Image).toBeVisible();
@@ -83,7 +75,6 @@ test.describe('Checkout tests', () => {
     })
 
     test('User should be successfully redirected to catalog page after pressing on Back Home button', async ({pageManager}) => {
-        await pageManager.cartPage.goToCheckoutViaCheckoutButton();
         await pageManager.checkoutPage.fillUserInfo(user);
         await pageManager.checkoutOverviewPage.finalizeOrder();
         await pageManager.checkoutCompletePage.returnToCatalog();
